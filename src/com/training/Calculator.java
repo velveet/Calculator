a514ab7fb29c30bb36b3629f2bd52259d8f86993
velveet <velveet@pm.me>
2021-01-22 08:59:47 -0500

    rebase advance_calculator to including base calculator.

--- src/com/training/Calculator.java
+++ src/com/training/Calculator.java
@@ -2,12 +2,21 @@ package com.training;
 
 public class Calculator {
 
+
     public double sum(double num1, double num2) {
         return num1 + num2;
     }
 
     public double subtract(double num1, double num2) {
         return num1 - num2;
+    }
+
+    public double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double division(double num1, double num2) {
+        return num1 / num2;
     }
 
 }
